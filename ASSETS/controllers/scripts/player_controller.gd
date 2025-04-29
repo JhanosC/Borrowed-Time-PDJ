c4a02ca0e3690390c2deedb30280b7ec1c604d20
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-04-29 13:02:20 -0300

    arrumado wall_run

--- ASSETS/controllers/scripts/player_controller.gd
+++ ASSETS/controllers/scripts/player_controller.gd
@@ -30,14 +30,8 @@ var crawling := false
 
 @export_subgroup("Movement Settings")
 @export var movement_speed := 15.0
-@export var air_cap := 0.85
-@export var air_accel := 800.0
-@export var air_move_speed := 500.0
 @export var desired_velocity := Vector3.ZERO
-@export var air_control := 0.5
-@export var ground_accel := 11.0
-@export var ground_decel := 7.0
-@export var ground_friction := 3.5
+@export var air_control := 2.0
 var direction := Vector3.ZERO
 var start_slide_speed := 15.0
 var wall_jump_counter := 0
@@ -50,7 +44,7 @@ var wall_normal: Vector3
 signal velocity_update(velocity: Vector3)
 signal fov_update(fov: float)
 signal camera_distortion_update(distortion: float)
-signal states_update(can_crouch,slaming,sliding,wall_running,on_floor: bool)
+signal states_update(can_crouch,slaming,sliding,wall_running: bool)
 signal position_update(x,y,z: float)
 
 @onready var camera = $CameraController/Camera3D
@@ -60,6 +54,7 @@ signal position_update(x,y,z: float)
 @onready var sliding_collision_shape: CollisionShape3D = $SlidingCollisionShape
 
 var debug_mode = true
+
 var dashing_timer = 0.0
 var dashing = false
 var dash_cooldown = 0.0
@@ -80,10 +75,8 @@ func _emit_debug_info():
 	velocity_update.emit(Vector3(velocity.x,0.,velocity.z).length())
 	fov_update.emit(camera.fov)
 	camera_distortion_update.emit(camera_distortion)
+	states_update.emit(can_crouch,slaming,sliding,wall_running,is_on_floor(),is_on_wall(),direction)
 	position_update.emit(position.x,position.y,position.z)
-	states_update.emit(
-		can_crouch,slaming,sliding,wall_running,is_on_floor(),is_on_wall(),direction
-		)
 
 func _push_away_rigid_bodies():
 	for i in get_slide_collision_count():
@@ -95,16 +88,13 @@ func _push_away_rigid_bodies():
 			
 			const MY_APPROX_MASS_KG = 60.0
 			var mass_ratio = min(1., MY_APPROX_MASS_KG / c.get_collider().mass)
-			
-			if mass_ratio < 0.25:
-				continue
 			push_dir.y = 0
+			
 			var push_force = mass_ratio
 			push_force = clamp(push_force, 0.0, 10.0)
 			c.get_collider().apply_impulse(
 				push_dir * velocity_diff_in_push_dir * push_force,
-				c.get_position() - c.get_collider().global_position
-				)
+				c.get_position() - c.get_collider().global_position)
 
 func _process_camera(_delta):
 	# Smooth camera movement
@@ -121,37 +111,28 @@ func _process_gravity(_delta):
 		self.velocity.y -= gravity * _delta
 
 func _handle_ground_physics(_delta):
-	if input_dir:
-		self.velocity.x = direction.x * get_move_speed()
-		self.velocity.z = direction.z * get_move_speed()
-	else:
-		self.velocity.x = move_toward(self.velocity.x, 0., movement_speed)
-		self.velocity.z = move_toward(self.velocity.z, 0., movement_speed)
-	if !dashing:
+	direction = lerp(direction, self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y), _delta * lerp_speed)
+	self.velocity.x = direction.x * get_move_speed()
+	self.velocity.z = direction.z * get_move_speed()
+	if !sliding:
 		_headbob_effect(_delta)
 
 func _handle_air_physics(_delta):
 	_process_gravity(_delta)
-	if input_dir:
-		desired_velocity.x = direction.x * get_move_speed()
-		desired_velocity.z = direction.z * get_move_speed()
-		self.velocity.x = move_toward(self.velocity.x, desired_velocity.x, air_control)
-		self.velocity.z = move_toward(self.velocity.z, desired_velocity.z, air_control)
-	if is_on_wall():
-		_wall_run(_delta)
-	else:
-		_stop_wall_run()
+	direction = lerp(direction, self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y), _delta * air_control)
+	self.velocity.x = direction.x * get_move_speed()
+	self.velocity.z = direction.z * get_move_speed()
 
 func _physics_process(_delta):
 	# Handle functions
 	handle_controls(_delta)
 	if is_on_floor():
 		wall_jump_counter = 0
-		if !sliding or crawling:
-			_handle_ground_physics(_delta)
+		_handle_ground_physics(_delta)
 	else:
 		_handle_air_physics(_delta)
 	
+	_wall_run(_delta)
 	_process_camera(_delta)
 	_distort_camera(_delta)
 	_push_away_rigid_bodies()
@@ -168,8 +149,7 @@ func _unhandled_input(event):
 func handle_controls(_delta):
 	# Get direction
 	input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
-	direction = lerp(direction, self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y), _delta * lerp_speed)
-		
+	
 	# Reload scene
 	if Input.is_action_just_pressed("reload"):
 		get_tree().call_deferred("reload_current_scene")
@@ -188,7 +168,7 @@ func handle_controls(_delta):
 	if is_on_floor() and !sliding:
 		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
 			self.velocity.y = jump_strength
-		
+	
 	if Input.is_action_just_pressed("dash") and !dashing and !sliding and dash_cooldown <= 0.0:
 		dashing = true
 		dashing_timer = 0.2
@@ -200,6 +180,7 @@ func handle_controls(_delta):
 		if dash_cooldown >= 0.0:
 			dash_cooldown -= _delta
 		dashing = false
+	
 	# Sliding and slam control
 	if Input.is_action_pressed("crouch") and can_crouch:
 		if is_on_floor() and input_dir:
@@ -229,16 +210,16 @@ func _stop_slide(_delta):
 	sliding_collision_shape.disabled = true
 
 func _wall_run(_delta):
-	if !sliding:
+	if is_on_wall() and !sliding and !is_on_floor():
 		wall_running = true
+		wall_normal = get_wall_normal()
 		if Input.is_action_just_pressed("jump") and wall_jump_counter < possible_wall_jumps:
-			wall_normal = get_wall_normal()
 			wall_jump_counter += 1
 			direction = wall_normal
 			self.velocity.y = jump_strength
 			can_wall_run = false
-func _stop_wall_run():
-	wall_running = false
+	else:
+		wall_running = false
 
 func _headbob_effect(_delta):
 	headbob_time += _delta * self.velocity.length()
