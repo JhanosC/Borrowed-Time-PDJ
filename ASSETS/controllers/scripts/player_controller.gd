329449426dc300687977400f100a37bff0144131
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-05-03 00:08:42 -0300

    organização de código

--- ASSETS/controllers/scripts/player_controller.gd
+++ ASSETS/controllers/scripts/player_controller.gd
@@ -15,7 +15,7 @@ var wall_friction := 0.05
 var possible_wall_jumps := 3
 var camera_distortion := -1.0
 var camera_distortion_strength := 0.6
-var camera_default_fov := 75.0
+var camera_default_fov := 0.0
 var camera_new_fov := camera_default_fov
 
 @export_subgroup("States")
@@ -39,9 +39,9 @@ var input_mouse: Vector2
 var input_dir: Vector2
 var wall_normal: Vector3
 
-signal velocity_update
-signal fov_update
-signal camera_distortion_update
+signal velocity_update(velocity: Vector3)
+signal fov_update(fov: float)
+signal camera_distortion_update(distortion: float)
 
 @onready var camera = $CameraController/Camera3D
 @onready var head: Node3D = $CameraController
@@ -49,14 +49,17 @@ signal camera_distortion_update
 @onready var standing_collision_shape: CollisionShape3D = $StandingCollisionShape
 @onready var sliding_collision_shape: CollisionShape3D = $SlidingCollisionShape
 
+var debug_mode = true
+
 func _ready():
 	gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+	camera_default_fov = camera.fov
 	for child in $WorldModel.find_children("*", "VisualInstance3D"):
 		child.set_layer_mask_value(1, false)
 		child.set_layer_mask_value(2, true)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _emit_degub_info():
+func _emit_debug_info():
 	# Emit info to UI
 	velocity_update.emit(Vector3(velocity.x,0.,velocity.z).length())
 	fov_update.emit(camera.fov)
@@ -75,32 +78,42 @@ func _push_away_rigid_bodies():
 			push_dir.y = 0
 			
 			var push_force = mass_ratio
+			push_force = clamp(push_force, 0.0, 10.0)
 			c.get_collider().apply_impulse(
 				push_dir * velocity_diff_in_push_dir * push_force,
 				c.get_position() - c.get_collider().global_position)
+func _process_gravity(_delta):
+	if is_on_floor():
+		return
+	elif wall_running and velocity.y <= 0:
+		self.velocity.y -= gravity * _delta * wall_friction
+	else:
+		self.velocity.y -= gravity * _delta
+
+func _process_camera(_delta):
+	# Smooth camera movement
+	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 70 * _delta, _delta * 5)	
+	camera.rotation.x = lerp_angle(camera.rotation.x, rotation_target.x, _delta * 50)
+	rotation.y = lerp_angle(rotation.y, rotation_target.y, _delta * 25)
+	
+	# Camera bobbing when landing
+	camera.position.y = lerp(camera.position.y, 0.0, _delta * 50)
+	if is_on_floor(): # Landed
+		camera.position.y = -0.1
+		wall_jump_counter = 0
 
 func _physics_process(_delta):
 	# Handle functions
 	handle_controls(_delta)
 	_wall_run(_delta)
-	
-	# Smooth camera movement
-	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 70 * _delta, _delta * 5)	
-	camera.rotation.x = lerp_angle(camera.rotation.x, rotation_target.x, _delta * 50)
-	rotation.y = lerp_angle(rotation.y, rotation_target.y, _delta * 25)
-
-	#C amera bobbing when landing
-	camera.position.y = lerp(camera.position.y, 0.0, _delta * 50)
-	if is_on_floor(): # Landed
-		camera.position.y = -0.1
-		wall_jump_counter = 0
+	_process_camera(_delta)
 	# Respawn
 	if position.y < -30:
 		get_tree().call_deferred("reload_current_scene")
 	_distort_camera(_delta)
 	_push_away_rigid_bodies()
 	move_and_slide()
-	_emit_degub_info()
+	_emit_debug_info()
 	
 func _unhandled_input(event):
 	# Mouse movement
@@ -128,8 +141,8 @@ func handle_controls(_delta):
 		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
 			self.velocity.y = jump_strength
 		_headbob_effect(_delta)
-	elif !wall_running:
-		self.velocity.y -= gravity * _delta
+	else:
+		_process_gravity(_delta)
 	
 	# Sliding and slam control
 	if Input.is_action_pressed("crouch") and can_crouch:
@@ -171,10 +184,6 @@ func _wall_run(_delta):
 	if is_on_wall() and !sliding and !is_on_floor():
 		wall_running = true
 		wall_normal = get_wall_normal()
-		if self.velocity.y <= 0:
-			self.velocity.y -= gravity * _delta * wall_friction
-		else:
-			self.velocity.y -= gravity * _delta
 		if Input.is_action_just_pressed("jump") and wall_jump_counter < possible_wall_jumps:
 			wall_jump_counter += 1
 			wish_dir = wall_normal
@@ -193,7 +202,7 @@ func _headbob_effect(_delta):
 
 # FOV when running or standing still for too long
 func _distort_camera(_delta):
-	if mouse_captured and velocity.length() <= 0.0 and false:# False for testing
+	if mouse_captured and velocity.length() <= 0.0 and !debug_mode:
 		camera_distortion += camera_distortion_strength * _delta
 		if camera_distortion >= 0.0:
 			camera_new_fov += camera_distortion
