319d45b50d0512a6290e7a1092affa8515a42da8
Jhanos <joao.sbardelotto@edu.pucrs.br>
2025-05-03 00:08:36 -0300

    wall run incompleto

--- ASSETS/controllers/scripts/player_controller.gd
+++ ASSETS/controllers/scripts/player_controller.gd
@@ -3,15 +3,19 @@ extends CharacterBody3D
 @export_subgroup("Properties")
 @export var jump_strength := 8.0
 @export var mouse_sensitivity = 700
+@export var air_control := 2.0
 const HEADBOB_MOVE_AMOUNT = 0.06
 const HEADBOB_FREQUENCY = 2.4
 var headbob_time := 0.0
 var lerp_speed := 20.0
 var sliding_height := 0.75
+var gravity := 0
 
 @export_subgroup("States")
 @export var auto_bhop := true
 @export var sliding = false
+@export var wall_running = false
+@export var can_wall_run = true
 
 @export_subgroup("Movement Settings")
 @export var movement_speed := 10.0
@@ -23,6 +27,7 @@ var movement_velocity: Vector3
 var rotation_target: Vector3
 var input_mouse: Vector2
 var input_dir: Vector2
+var wall_normal
 
 signal velocity_update
 
@@ -33,6 +38,7 @@ signal velocity_update
 @onready var sliding_collision_shape: CollisionShape3D = $SlidingCollisionShape
 
 func _ready():
+	gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 	for child in $WorldModel.find_children("*", "VisualInstance3D"):
 		child.set_layer_mask_value(1, false)
 		child.set_layer_mask_value(2, true)
@@ -58,14 +64,7 @@ func _push_away_rigid_bodies():
 func _physics_process(delta):
 	#Handle functions
 	handle_controls(delta)
-	if is_on_floor():
-		lerp_speed = 15.0
-		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
-			self.velocity. y = jump_strength
-		_headbob_effect(delta)
-	else:
-		lerp_speed = 2.0
-		self.velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity") * delta
+	_wall_run(delta)
 	
 	#Smooth camera movement
 	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 70 * delta, delta * 5)	
@@ -101,23 +100,37 @@ func handle_controls(_delta):
 		input_mouse = Vector2.ZERO
 	rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
 	
+	if is_on_floor() and !sliding:
+		if Input.is_action_just_pressed("jump") or (auto_bhop and Input.is_action_pressed("jump")):
+			self.velocity.y = jump_strength
+		_headbob_effect(_delta)
+	elif !wall_running:
+		self.velocity.y -= gravity * _delta
+	if self.velocity.y <= 0:
+		can_wall_run = true
+	else:
+		can_wall_run = false
+	
 	if Input.is_action_pressed("crouch"):
 		_slide(_delta)
 	elif !raycast.is_colliding() and is_on_floor():
 		_stop_slide(_delta)
+	
 	#Get direction
+	input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
 	if !sliding and is_on_floor():
-		input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward").normalized()
 		wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y)
+	elif !is_on_floor() and !wall_running:
+		wish_dir = lerp(wish_dir, self.global_transform.basis * Vector3(input_dir.x, 0., input_dir.y), _delta * air_control)
 	if wish_dir:
-		velocity.x = wish_dir.x * movement_speed
-		velocity.z = wish_dir.z * movement_speed
+		self.velocity.x = wish_dir.x * movement_speed
+		self.velocity.z = wish_dir.z * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, movement_speed)
-		velocity.z = move_toward(velocity.z, 0, movement_speed)
+		self.velocity.x = move_toward(self.velocity.x, 0, movement_speed)
+		self.velocity.z = move_toward(self.velocity.z, 0, movement_speed)
 	
 	# Emit velocity to UI
-	velocity_update.emit(velocity.length())
+	velocity_update.emit(self.velocity.length())
 
 func _slide(delta):
 	head.position.y = lerp(head.position.y, sliding_height, delta * lerp_speed)
@@ -137,3 +150,15 @@ func _headbob_effect(delta):
 		sin(headbob_time * HEADBOB_FREQUENCY) * HEADBOB_MOVE_AMOUNT,
 		0
 	)
+	
+func _wall_run(delta):
+	if is_on_wall() and !sliding and can_wall_run:
+		wall_running = true
+		wall_normal = get_wall_normal()
+		#wish_dir = -wall_normal
+		self.velocity.y -= gravity * delta * 0.1
+		if Input.is_action_just_pressed("jump"):
+			var wall_jump_direction = (wall_normal + Vector3.UP).normalized()
+			self.velocity = wall_jump_direction * jump_strength
+	else:
+		wall_running = false
